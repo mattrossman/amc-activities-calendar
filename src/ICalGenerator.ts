e66fed06aa75ab6999c53a06abf97d2138c99c6f
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2025-03-31 23:15:34 -0400

    ICalGenerator

---
+++ src/ICalGenerator.ts
@@ -0,0 +1,85 @@
+import { DateTime, Effect } from "effect"
+import ical from "ical-generator"
+import { DOMParser } from "@xmldom/xmldom"
+
+import type * as Activity from "~/Activity"
+
+const formatLocation = (activity: Activity.Activity) => {
+  const lines: string[] = []
+
+  if (activity.Start_Location__c?.street) {
+    lines.push(activity.Start_Location__c.street)
+  }
+
+  let line2 = ""
+
+  if (activity.Start_Location__c?.city) {
+    line2 = activity.Start_Location__c.city
+  }
+
+  if (activity.Start_Location__c?.state) {
+    line2 = `${line2}, ${activity.Start_Location__c.state}`
+  }
+
+  if (activity.Start_Location__c?.postalCode) {
+    line2 = `${line2} ${activity.Start_Location__c.postalCode}`
+  }
+
+  if (line2.length > 0) {
+    lines.push(line2)
+  }
+
+  return lines.join("\n")
+}
+
+const formatDescription = (activity: Activity.Activity) => {
+  const description = new DOMParser().parseFromString(
+    `<!DOCTYPE html><html>${activity.Description__c}</html>`,
+    "text/html"
+  ).textContent
+
+  const start = activity.Start_Concatenation_Formula_Unconverted__c
+
+  let result = start
+
+  if (description) {
+    result += `\n${description}`
+  }
+
+  return result
+}
+
+export class ICalGenerator extends Effect.Service<ICalGenerator>()(
+  "ICalGenerator",
+  {
+    effect: Effect.gen(function* () {
+      const fromActivities = Effect.fn("fromActivities")(function* (
+        activities: typeof Activity.Activities.Type
+      ) {
+        const cal = ical()
+
+        for (const activity of activities) {
+          const start = yield* DateTime.make(activity.Start_Date__c).pipe(
+            Effect.map(DateTime.toDate)
+          )
+
+          const location = formatLocation(activity)
+          const description = formatDescription(activity)
+
+          cal.createEvent({
+            start,
+            allDay: true,
+            summary: activity.Activity_Name__c,
+            location: location.length > 0 ? location : null,
+            description:
+              description && description.length > 0 ? description : null,
+          })
+        }
+
+        return cal.toString()
+      })
+
+      return { fromActivities }
+    }),
+  }
+) {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
-import { NodeRuntime, NodeKeyValueStore } from "@effect/platform-node"
+import {
+  NodeRuntime,
+  NodeKeyValueStore,
+  NodeContext,
+} from "@effect/platform-node"
+import { NodeSdk } from "@effect/opentelemetry"
 import { Cause, Effect, Layer } from "effect"
+import { PersistedCache, Persistence } from "@effect/experimental"
+import { FileSystem } from "@effect/platform"
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base"
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http"
 
 import * as ActivityScraper from "~/ActivityScraper"
-import { PersistedCache, Persistence } from "@effect/experimental"
+import * as ICalGenerator from "~/ICalGenerator"
+
+const NodeSdkLive = NodeSdk.layer(() => ({
+  resource: { serviceName: "effect" },
+  spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter()),
+}))
 
 const MainLayer = Layer.mergeAll(
   ActivityScraper.ActivityScraper.Default,
+  ICalGenerator.ICalGenerator.Default,
+  NodeContext.layer,
+  NodeSdkLive,
   Persistence.layerResultKeyValueStore.pipe(
     Layer.provide(NodeKeyValueStore.layerFileSystem("./ignore/cache"))
   )
@@ -36,6 +53,13 @@ const program = Effect.gen(function* () {
       url: "https://activities.outdoors.org/s/?chapters=0015000001Sg069AAB&audiences=20%E2%80%99s+%26+30%E2%80%99s",
     })
   )
+
+  const icalGenerator = yield* ICalGenerator.ICalGenerator
+  const ical = yield* icalGenerator.fromActivities(activities)
+
+  const path = "./ignore/activities.ics"
+  const fs = yield* FileSystem.FileSystem
+  yield* fs.writeFileString(path, ical)
 
   const leader = activities.at(0)?.OC_Trip_Leaders__r.at(0)
 
